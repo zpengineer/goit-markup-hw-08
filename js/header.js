08e13e5ec1728f1224352f7bea84a8962c853d31
Oleh Fedorov <clickprime94@gmail.com>
2021-10-13 15:59:53 +0300

    V1

---
+++ js/header.js
@@ -0,0 +1,9 @@
+(() => {
+
+  const { height: pageHeaderHeight } = document
+    .querySelector(".page-header")
+    .getBoundingClientRect();
+  
+    document.body.style.paddingTop = `${pageHeaderHeight}px`;
+    
+})();
